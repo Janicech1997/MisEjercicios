6b82c22bf0338c2e9b5230c971e8af41448b85e7
Janice Chen <janicechen31997@hotmail.com>
2022-12-22 09:20:19 -0500

    version1

---
+++ Ejercicio 1/index.js
@@ -0,0 +1,24 @@
+console.log('Hola desde Js')
+
+
+function encabezado() {
+  // let encabezado = document.createElement('h1')
+  // let texto = document.createTextNode('Soy un encabezado')
+  // encabezado.appendChild(texto)
+  // encabezado.id = 'encabezado'
+  // encabezado.className = 'soy-una-clase'
+  // document.querySelector('body').appendChild(encabezado)
+  // console.log(document.getElementById('encabezado').classList)
+  let contenedor = document.querySelector('body')
+
+  contenedor.innerHTML += `<h1 id="encabezado">Soy un encabezado 2.0</h1>`
+
+}
+function parrafo() {
+  let parrafo = document.createElement('h1')
+  let texto = document.createTextNode('Soy un parrafo')
+  parrafo.appendChild(texto)
+  parrafo.id = 'parrafo'
+  document.querySelector('body').appendChild(parrafo)
+  console.log(document.getElementById('parrafo').classList)
+}
---
+++ Ejercicio 3/index.js
@@ -0,0 +1,51 @@
+console.log('Hola desde Js')
+
+function clicked (event) {
+  console.log('click desde la funcion')
+  alert('Hola soy un alert')
+  console.log(event)
+}
+
+// localStorage 
+localStorage.setItem('miGato', 'Juan')
+let cat = localStorage.getItem('miGato')
+console.log(cat)
+// localStorage.removeItem('miGato')
+// localStorage.clear()
+
+function guardar() {
+  let inputValue = document.getElementById('inputText').value
+  let nombres = JSON.parse(localStorage.getItem('nombres')) ? JSON.parse(localStorage.getItem('nombres')) : []
+  console.log(inputValue)
+  nombres.push(inputValue)
+  console.log(nombres)
+  console.log(JSON.stringify(nombres))
+  localStorage.setItem('nombres', JSON.stringify(nombres))
+  document.getElementById('inputText').value = ''
+  render()
+}
+
+function render() {
+  let lista = document.getElementById('lista')
+  let nombres = JSON.parse(localStorage.getItem('nombres')) ? JSON.parse(localStorage.getItem('nombres')) : [] 
+  console.log(nombres)
+  lista.innerHTML = ''
+  nombres.forEach((element, index) => {
+    console.log(index)
+    lista.innerHTML += `
+      <p> ${element} </p>
+      <button onclick="borrar(${index})" >x</button>
+    `
+  });
+}
+
+function borrar(position) {
+  let nombres = JSON.parse(localStorage.getItem('nombres')) ? JSON.parse(localStorage.getItem('nombres')) : [] 
+  console.log(nombres.splice(position, 1))
+  console.log(JSON.stringify(nombres))
+  localStorage.setItem('nombres', JSON.stringify(nombres))
+  render()
+  console.log('borrado')
+}
+
+render()
